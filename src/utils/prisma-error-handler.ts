25f23cad8f77a3b4875f0253a884261016076b2e
Nearo007 <araujo8166@gmail.com>
2026-04-20 13:47:46 -0300

    refactor(prisma): enhance error handling for unique constraint violations in Prisma error handler

--- src/utils/prisma-error-handler.ts
+++ src/utils/prisma-error-handler.ts
@@ -1,20 +1,40 @@
-import { Prisma } from "../generated/prisma/client.js";
 import { MESSAGES } from "../constants/messages.js";
+import { Prisma } from "../generated/prisma/client.js";
 
 export const handlePrismaError = (err: any): never => {
     if (err instanceof Prisma.PrismaClientKnownRequestError) {
         switch (err.code) {
-            case "P2002": { // Unique constraint
-                const field = (err.meta?.target as string[])?.includes("email") ? "EMAIL" : "NAME";
-                console.log(field)
-                throw new Error(MESSAGES.USER.CONFLICT[`${field}_EXISTS`]);
+            case "P2002": {
+                const meta = err.meta as {
+                    driverAdapterError?: {
+                        cause?: {
+                            constraint?: {
+                                fields?: string[];
+                            };
+                        };
+                    };
+                    target?: string[];
+                };
+
+                const fields = meta.target || meta.driverAdapterError?.cause?.constraint?.fields;
+                const field = fields?.[0];
+
+                if (field === "name") {
+                    throw new Error(MESSAGES.USER.CONFLICT.NAME_EXISTS);
+                }
+
+                if (field === "email") {
+                    throw new Error(MESSAGES.USER.CONFLICT.EMAIL_EXISTS);
+                }
+                
+                throw new Error("Conflito de restrição única.");
             }
-            case "P2025": // Record not found
+            case "P2025":
                 throw new Error(MESSAGES.USER.NOT_FOUND.GENERAL);
             default:
                 throw new Error(`Erro de banco de dados: ${err.code}`);
         }
     }
-    
+
     throw err;
 };
